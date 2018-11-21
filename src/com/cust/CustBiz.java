994cbc18847747db9d4dc8574194f0f0f9439906
student <student@M7045>
2018-11-21 16:43:39 +0900

    d02222

---
+++ src/com/cust/CustBiz.java
@@ -0,0 +1,50 @@
+package com.cust;
+
+import java.util.ArrayList;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+
+import com.frame.Biz;
+import com.frame.Dao;
+
+@Component("cbiz")
+public class CustBiz implements Biz<String, Cust> {
+	@Resource(name="cdao")
+	Dao dao;
+		
+	public Dao getDao() {
+		return dao;
+	}
+
+	public void setDao(Dao dao) {
+		this.dao = dao;
+	}
+
+	@Override
+	public void register(Cust v) {
+		dao.insert(v);
+	}
+
+	@Override
+	public void remove(String k) {
+		dao.delete(k);
+	}
+
+	@Override
+	public void modify(Cust v) {
+		dao.update(v);
+	}
+
+	@Override
+	public Cust get(String k) {
+		return (Cust) dao.select(k);
+	}
+
+	@Override
+	public ArrayList<Cust> get() {
+		return dao.select();
+	}
+
+}
